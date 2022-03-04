02bce6c20a3560f974d9060fbae75c5697585b3f
thomas-star <60140141+thomas-star@users.noreply.github.com>
2022-03-04 04:55:30 +0100

    Update generator.java

--- generator.java
+++ generator.java
@@ -87,7 +87,12 @@ public class generator {
             wr.println(
                     "<!DOCTYPE html>  <head> <title>GO Sécuri</title> <link rel='stylesheet' href='style.css'/> </head>");
             wr.println(
-                    "<body class='bck'> <div> <img src='/png/GOS.png' alt=''><div class='round'> <div class='ma'> </div> </div></div>  </body>");
+                    "<body class='bck'> <div> <img src='/png/GOS.png' alt=''>/*</div>  </body>*/");
+            wr.println("<div class='round'> <div class='ma'>");
+                String Nom = nom.substring(0, 1).toUpperCase() + nom.substring(1);
+                wr.println("<tr><a href='txt/" + nom + ".html'>" + Nom + "</a></tr>");
+                wr.println("</div> </div>");
+            wr.println("</div>  </body>");
             wr.println("<footer> <p>*GO Sécuri tous droits réservé Entreprise titulaire </p> </footer> </html>");
 
             for (String nom : staff) {
@@ -103,10 +108,10 @@ public class generator {
                     System.out.println(err);
                 }
 
-                //wr.println("<div class='round'> <div class='ma'>");
+                /*wr.println("<div class='round'> <div class='ma'>");
                 String Nom = nom.substring(0, 1).toUpperCase() + nom.substring(1);
                 wr.println("<tr><a href='txt/" + nom + ".html'>" + Nom + "</a></tr>");
-                //wr.println("</div> </div>");
+                wr.println("</div> </div>");*/
                 
 
                 String fileName2 = "html/txt/" + nom + ".html";
