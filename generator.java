f8ecfb65f1ff9282e9b2eaafad334b142e188867
Yuzux <61511020+Yuzux@users.noreply.github.com>
2022-03-10 17:49:11 +0100

    Update generator.java

--- generator.java
+++ generator.java
@@ -115,7 +115,7 @@ public class generator {
 
                 wr2.println("<!doctype html>  <html lang='fr'> <head> <meta charset='utf-8'> <title>GO Securi</title> <link rel='stylesheet' href='style2.css'> <link rel='icon' href='favicon.ico' /> </head>");
                 wr2.println("<body class='bck'><img src='/png/GOS.png' alt='' class='cen'> <div class='round'> <div class='ma'> <div class='nme'><h1>" + Nom + "</h1></div>");
-                wr2.println("<img src='/png/" + nom + ".png' class='CNI'></div> </div>");
+                wr2.println("<img src='/png/" + nom + ".png' class='CNI'></div>");
                 co = 0;
                 try {
                     FileInputStream f2 = new FileInputStream("html/txt/" + nom + ".txt");
@@ -164,7 +164,7 @@ public class generator {
                     }
                 }
                 wr2.println("</div>");
-                wr2.println("</td> </tr> </table> </div> </body> </html>");
+                wr2.println("</div> </body> </html>");
                 wr2.close();
             }
             wr.println("</div></div></body>");
